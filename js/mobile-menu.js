33fe45fa2aec8ef8678372dd0b726f84fec343d7
CeresRasmussen <112823848+CeresRasmussen@users.noreply.github.com>
2022-11-20 23:46:52 +0200

    Almost done?

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,6 +1,7 @@
 (() => {
   const menuBtnRef = document.querySelector("[data-menu-button]");
   const mobileMenuRef = document.querySelector("[data-menu]");
+  const mobileMenuBody = document.querySelector('[data-menu-open]');
 
   menuBtnRef.addEventListener("click", () => {
     const expanded =
@@ -8,6 +9,8 @@
 
     menuBtnRef.classList.toggle("is-open");
     menuBtnRef.setAttribute("aria-expanded", !expanded);
+
+    mobileMenuBody.classList.toggle("menu-open");
 
     mobileMenuRef.classList.toggle("is-open");
   });
